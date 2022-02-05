a872519f72b1747c25e9b1fe3bd92c12a8687145
Patrick Holmquist <patrickjholmquist@gmail.com>
2022-02-05 10:19:30 -0800

    done

--- .history/Earthquake_Challenge/static/js/challenge_logic_20220205101518.js
+++ .history/Earthquake_Challenge/static/js/challenge_logic_20220205101518.js
@@ -23,12 +23,15 @@ let satelliteStreets = L.tileLayer(
   }
 );
 
-// We create the dark view tile layer that will be an option for our map.
-let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+let darkMode = L.tileLayer(
+  "https://api.mapbox.com/styles/v1/mapbox//tiles/{z}/{x}/{y}?access_token={accessToken}",
+  {
+    attribution:
+      'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
-    accessToken: API_KEY
-});
+    accessToken: API_KEY,
+  }
+);
 
 // Create the map object with center, zoom level and default layer.
 let map = L.map("mapid", {
@@ -41,7 +44,6 @@ let map = L.map("mapid", {
 let baseMaps = {
   Streets: streets,
   Satellite: satelliteStreets,
-  Dark: dark
 };
 
 // 1. Add a 2nd layer group for the tectonic plate data.
@@ -51,7 +53,7 @@ let MajorQuakes = new L.LayerGroup(streets, satelliteStreets);
 
 // 2. Add a reference to the tectonic plates group to the overlays object.
 let overlays = {
-  "Earthquakes": allEarthquakes,
+  Earthquakes: allEarthquakes,
   "Tectonic Plates": tec,
   "Major Earthquakes": MajorQuakes,
 };
